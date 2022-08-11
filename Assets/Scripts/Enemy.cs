ea32ad86eab12467d97fe8c6b218a19d13b1e0ed
Sedjex <kathshem@gmail.com>
2022-08-11 12:49:14 +0300

    Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //enemy health
+    public int EnemyHealth;
+    //taking damage
+    public void GetDamage(int damage)
+    {
+        EnemyHealth -= damage;
+        if (EnemyHealth <= 0)
+        {
+            Destruction();
+        }
+    }
+
+    private void Destruction()
+    {
+        Destroy(gameObject);
+    }
+
+    //if eneme collides player - gets the damage
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            GetDamage(1);
+        }
+    }
+}
