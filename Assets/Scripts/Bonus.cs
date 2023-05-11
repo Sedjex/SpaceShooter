dabd3457eb583b77a09b928dc9013e8d257a203c
Sedjex <kathshem@gmail.com>
2023-05-11 14:23:03 +0300

    bonuses

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            // If the current power level of the guns is not the maximum...
+            if (PlayerShooting.instance.cur_Power_Level_Guns < PlayerShooting.instance.max_Power_Level_Guns)
+            {
+                // Increase the current power level of guns.
+                PlayerShooting.instance.cur_Power_Level_Guns++;
+            }
+            // Destroy the current Bonus object.
+            Destroy(gameObject);
+        }
+    }
+}
