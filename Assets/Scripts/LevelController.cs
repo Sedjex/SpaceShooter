d1642773f7aa6ca44523bcb0441bdd6637ae3292
Sedjex <kathshem@gmail.com>
2023-06-21 12:34:38 +0300

    sliders

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,18 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 [System.Serializable]
 public class EnemyWaves
 {
-    // Time when the wave will appear on the stage 
+    // Time when the wave will appear on the stage
     public float timeToStart;
     // The Enemy Wave prefab to be spawned.
     public GameObject wave;
     // This wave after which the game will end.
-    public bool isLastWave;
+    public bool is_Last_Wave;
 }
 
 public class LevelController : MonoBehaviour
@@ -23,6 +20,8 @@ public class LevelController : MonoBehaviour
     public GameObject[] playerShip;
     // Reference to the EnemyWaves.
     public EnemyWaves[] enemyWaves;
+    // Finish the game or not
+    private bool is_Final = false;
 
     private void Awake()
     {
@@ -42,16 +41,34 @@ public class LevelController : MonoBehaviour
         for (int i = 0; i < enemyWaves.Length; i++)
         {
             // Start CreateEnemyWave as a coroutine.
-            StartCoroutine(CreateEnemyWave(enemyWaves[i].timeToStart, enemyWaves[i].wave));
+            StartCoroutine(CreateEnemyWave(enemyWaves[i].timeToStart, enemyWaves[i].wave, enemyWaves[i].is_Last_Wave));
         }
     }
 
-    IEnumerator CreateEnemyWave(float delay, GameObject Wave)
+    private void Update()
+    {
+
+        // If is_Final = true, there are no objects with the Enemy tag left...
+        if (is_Final == true && GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            // You win game!
+            Debug.Log("Win");
+        }
+        if (Player.instance == null)
+        {
+            // You lose game!
+            Debug.Log("Lose");
+        }
+    }
+    IEnumerator CreateEnemyWave(float delay, GameObject Wave, bool Final)
     {
         // if creation wave time ! = 0 and player is alive ...create a wave
         if (delay != 0)
             yield return new WaitForSeconds(delay);
         if (Player.instance != null)
             Instantiate(Wave);
+        // If this wave is the last...
+        if (Final == true)
+            is_Final = true;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,6 +13,11 @@ public class Player : MonoBehaviour
     // Shield health.
     public int shield_Health = 1;
 
+    // Reference private to the UI's health bar.
+    private Slider _slider_hp_Player;
+    // Reference private to the UI's shield bar.
+    private Slider _slider_hp_Shield;
+
     private void Awake()
     {
         // Setting up the references.
@@ -26,29 +29,40 @@ public class Player : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        // Look for an object with tag hp_Player and take the Slider component from it
+        _slider_hp_Player = GameObject.FindGameObjectWithTag("SlHP").GetComponent<Slider>();
+        // Look for an object with tag hp_Shield and take the Slider component from it
+        _slider_hp_Shield = GameObject.FindGameObjectWithTag("SlShield").GetComponent<Slider>();
     }
 
-    private void Start()
-    {
-        // If the shield has life...
-        if (shield_Health != 0)
-        {
-            //Show shield.
-            obj_Shield.SetActive(true);
-        }
-        // If the shield has no life...
-        else
-        {
-            // Hide shield.
-            obj_Shield.SetActive(false);
-        }
-    }
+    //private void Start()
+    //{
+    //    // Set the health bar's value to the current health.
+    //    _slider_hp_Player.value = (float)player_Health / 15; //slider has a range from 0 to 1
+    //    // If the shield has life...
+    //    if (shield_Health != 0)
+    //    {
+    //        //Show shield.
+    //        obj_Shield.SetActive(true);
+    //        // Set the shield bar's value to the current health.
+    //        _slider_hp_Shield.value = (float)shield_Health / 6; //slider has a range from 0 to 1
+    //    }
+    //    // If the shield has no life...
+    //    else
+    //    {
+    //        // Hide shield.
+    //        obj_Shield.SetActive(false);
+    //        _slider_hp_Shield.value = 0;
+    //    }
+    //}
 
     // Method of taking damage by the shield
     public void GetDamageShield(int damage)
     {
         // Reduce the shield health by the damage amount.
         shield_Health -= damage;
+        // Update shield bar's value to the current shield health.
+        _slider_hp_Shield.value = (float)shield_Health / 10;
 
         // If the shield does not have a health...
         if (shield_Health <= 0)
@@ -63,6 +77,8 @@ public class Player : MonoBehaviour
     {
         // Reduce the health by the damage amount.
         player_Health -= damage;
+        // Update health bar's value to the current health.
+        _slider_hp_Player.value = (float)player_Health / 10;
 
         // If the player does not have a health...
         if (player_Health <= 0)
