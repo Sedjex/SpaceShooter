ff68775aeeb14b94b1395b566bd70c19d03e34ba
Sedjex <kathshem@gmail.com>
2023-06-23 14:28:46 +0300

    Particles

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Guns
 {
     public GameObject obj_Central_Gun, obj_Right_Gun, obj_Left_Gun;
+    public ParticleSystem ps_Central_Gun, ps_Left_Gun, ps_Right_Gun;
 }
 
 public class PlayerShooting : MonoBehaviour
@@ -38,7 +39,16 @@ public class PlayerShooting : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    //Права на данный курс принадлежат Дорофеевой Карине Олеговне, данный курс создавался для Udemy сайта
+    private void Start()
+    {
+        // Try and find an ParticleSystem Component on the gameobject gun central.
+        guns.ps_Central_Gun = guns.obj_Central_Gun.GetComponent<ParticleSystem>();
+        // Try and find an ParticleSystem Component on the gameobject gun left.
+        guns.ps_Left_Gun = guns.obj_Left_Gun.GetComponent<ParticleSystem>();
+        // Try and find an ParticleSystem Component on the gameobject gun right.
+        guns.ps_Right_Gun = guns.obj_Right_Gun.GetComponent<ParticleSystem>();
+    }
+
     private void Update()
     {
         //every timer_Shot seconds we call the method MakeAShot
@@ -63,17 +73,23 @@ public class PlayerShooting : MonoBehaviour
             //One gun shoot
             case 1:
                 CreateBullet(obj_Bullet, guns.obj_Central_Gun.transform.position, Vector3.zero);
+                guns.ps_Central_Gun.Play();
                 break;
             //two guns shoots
             case 2:
                 CreateBullet(obj_Bullet, guns.obj_Right_Gun.transform.position, Vector3.zero);
                 CreateBullet(obj_Bullet, guns.obj_Left_Gun.transform.position, Vector3.zero);
+                guns.ps_Right_Gun.Play();
+                guns.ps_Left_Gun.Play();
                 break;
             //three guns shoots
             case 3:
                 CreateBullet(obj_Bullet, guns.obj_Central_Gun.transform.position, Vector3.zero);
                 CreateBullet(obj_Bullet, guns.obj_Right_Gun.transform.position, new Vector3(0, 0, -5));
                 CreateBullet(obj_Bullet, guns.obj_Left_Gun.transform.position, new Vector3(0, 0, 5));
+                guns.ps_Right_Gun.Play();
+                guns.ps_Left_Gun.Play();
+                guns.ps_Central_Gun.Play();
                 break;
             //five guns shoots
             case 4:
@@ -82,6 +98,9 @@ public class PlayerShooting : MonoBehaviour
                 CreateBullet(obj_Bullet, guns.obj_Right_Gun.transform.position, new Vector3(0, 0, 5));
                 CreateBullet(obj_Bullet, guns.obj_Left_Gun.transform.position, new Vector3(0, 0, 0));
                 CreateBullet(obj_Bullet, guns.obj_Left_Gun.transform.position, new Vector3(0, 0, -5));
+                guns.ps_Right_Gun.Play();
+                guns.ps_Left_Gun.Play();
+                guns.ps_Central_Gun.Play();
                 break;
             //five guns shoots. Another shot mode.
             case 5:
@@ -90,6 +109,9 @@ public class PlayerShooting : MonoBehaviour
                 CreateBullet(obj_Bullet, guns.obj_Right_Gun.transform.position, new Vector3(0, 0, -15));
                 CreateBullet(obj_Bullet, guns.obj_Left_Gun.transform.position, new Vector3(0, 0, 5));
                 CreateBullet(obj_Bullet, guns.obj_Left_Gun.transform.position, new Vector3(0, 0, 15));
+                guns.ps_Right_Gun.Play();
+                guns.ps_Left_Gun.Play();
+                guns.ps_Central_Gun.Play();
                 break;
         }
     }
